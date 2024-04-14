990769564e4771f6c354f91221126c6dd92969b2
Eliel Silva <hydrahtec@gmail.com>
2024-04-14 15:21:44 -0300

    docs: python projeto calculadora de tinta, arquivo calculadora - declaração de proprieadades e construção de métodos

--- calculadora_tinta/main.py
+++ calculadora_tinta/main.py
@@ -1,15 +1,22 @@
+from calculadora_mod import Calculadora
+calc = Calculadora()
+
 largura = float(input('Qual a largura do comodo? '))
 altura = 2.9
 profundidade = float(input('Qual a profundidade do comodo? '))
 
-area_paredes: float = 2 * (largura + profundidade) * altura
+#calc.area_parede = 2 * (largura + profundidade) * altura
 
-print('A area das paredes é: ', area_paredes, 'M²')
+print('A area das paredes é: ', 
+      calc.calcular_area_paredes(largura, profundidade, altura), 'M²')
 
-area_teto: float = largura * altura
+#calc.area_teto = largura * altura
 
-print('A area do teto é: ', area_teto, 'M²')
+print('A area do teto é: ', 
+      calc.calcular_area_teto(largura, profundidade), 'M²')
 
 print('A litragem de tint necessaria é: ',
-      (area_paredes + area_teto) / 10, 'M²'
+      calc.calcular_litragem_necessaria(), 'M²'
       )
+
+# aula 7
