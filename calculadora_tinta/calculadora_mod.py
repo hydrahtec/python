c46f9c33dd01c665ba357e71acd75934f9b4fdee
Eliel Silva <hydrahtec@gmail.com>
2024-04-15 20:14:26 -0300

    docs: python projeto calculo de tinta, metodos getter e setter

--- calculadora_tinta/calculadora_mod.py
+++ calculadora_tinta/calculadora_mod.py
@@ -1,14 +1,14 @@
 class Calculadora:
-    area_parede: float
-    area_teto: float
+    __area_parede: float
+    __area_teto: float
 
-    def calcular_area_paredes(self, largura, profundidade, altura):
-        self.area_parede = 2 * (largura + profundidade) * altura
-        return self.area_parede
+    def calcular_area_paredes(self, comodo):
+        self.__area_parede = 2 * (comodo.largura + comodo.profundidade) * comodo.altura
+        return self.__area_parede
     
-    def calcular_area_teto(self, largura, profundidade):
-        self.area_teto = largura * profundidade
-        return self.area_teto
+    def calcular_area_teto(self, comodo):
+        self.__area_teto = comodo.largura * comodo.profundidade
+        return self.__area_teto
     
     def calcular_litragem_necessaria(self):
-        return (self.area_parede + self.area_teto) / 10
+        return (self.__area_parede + self.__area_teto) / 10
--- calculadora_tinta/main.py
+++ calculadora_tinta/main.py
@@ -7,12 +7,12 @@ comodo = Comodo(input('Qual a largura do comodo? '), input('Qual a profundidade 
 #calc.area_parede = 2 * (largura + profundidade) * altura
 
 print('A area das paredes é: ', 
-      calc.calcular_area_paredes(comodo.largura, comodo.profundidade, comodo.altura), 'M²')
+      calc.calcular_area_paredes(comodo), 'M²')
 
 #calc.area_teto = largura * altura
 
 print('A area do teto é: ', 
-      calc.calcular_area_teto(comodo.largura, comodo.profundidade), 'M²')
+      calc.calcular_area_teto(comodo), 'M²')
 
 print('A litragem de tint necessaria é: ',
       calc.calcular_litragem_necessaria(), 'M²'
